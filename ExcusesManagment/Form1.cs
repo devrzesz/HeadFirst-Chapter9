8201edd86f30ecab78376a54c690438b0f1faa51
Ariel Rzeszowski <rzeszowsky@gmail.com>
2019-06-03 10:39:01 +0200

    Kolejne zmiany :) Przeniesiona logika biznesowa do LibrararyClassTest ;)

--- ExcusesManagment/Form1.cs
+++ ExcusesManagment/Form1.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibraryClassTest;
 
 namespace ExcusesManagment
 {
@@ -18,6 +12,7 @@ namespace ExcusesManagment
         private bool formChanged = false;
         private string selectedFolder = "";
 
+        
         public Form1()
         {
             InitializeComponent();
--- LibraryClassTest/ExcusesManagment/Excuse.cs
+++ LibraryClassTest/ExcusesManagment/Excuse.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace ExcusesManagment
+namespace LibraryClassTest
 {
-    class Excuse
+    public class Excuse
     {
         public string Description { get; set; }
         public string Results { get; set; }
@@ -31,12 +30,12 @@ namespace ExcusesManagment
             OpenFile(fileNames[random.Next(fileNames.Length)]);
         }
 
-        
+
 
         public void OpenFile(string excusePath)
         {
             this.ExcusePath = excusePath;
-            using(StreamReader reader = new StreamReader(excusePath))
+            using (StreamReader reader = new StreamReader(excusePath))
             {
                 Description = reader.ReadLine();
                 Results = reader.ReadLine();
