7211be4c9d21624e2afa6c62879e2ad9ad4838f6
Ariel Rzeszowski <rzeszowsky@gmail.com>
2019-06-03 10:19:46 +0200

    Tym razem usunąłem jeden projekt oraz dodałem biblioteke klas do rozwiązania

---
+++ LibraryClassTest/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryClassTest
+{
+    public class Class1
+    {
+    }
+}
